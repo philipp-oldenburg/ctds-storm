7d97a024e61e57693e6a5cebb1bf370845daf2b3
Philipp Oldenburg <philipp.oldenburg@stud.unibas.ch>
2015-11-05 02:04:07 +0100

    worked on sensor server

--- Project Weather Station/SensorServer/SensorServer.py
+++ Project Weather Station/SensorServer/SensorServer.py
@@ -1,8 +1,9 @@
 import socket
 import thread
+import Adafruit_BMP.BMP085 as BMP085
 
 
-def readline(socket):  # ignores \r
+def readline(): # ignores \r
     line = "";
     while True:
         byte = socket.recv(1)
@@ -14,23 +15,28 @@ def readline(socket):  # ignores \r
             line += byte
 
 
-def handler(conn, addr):
+def handler():
     try:
         print "Got connection from:", addr
         while True:
             msg = readline(conn)
             if msg == "TEMP":
-                conn.send("0.001" + '\n')
+                conn.send('{0:0.2f}\n'.format(sensor.read_temperature()))
             elif msg == "PRES":
-                conn.send("0.002" + '\n')
+                conn.send('{0:0.2f}\n'.format(sensor.read_pressure()))
             elif msg == "ALTI":
-                conn.send("0.003" + '\n')
+                conn.send('{0:0.2f}\n'.format(sensor.read_altitude()))
+            elif msg == "SEAL":
+                conn.send('{0:0.2f}\n'.format(sensor.read_sealevel_pressure()))
+            else:
+                print "received message in conflict with our protocol"
     except Exception as e:
         print e.message
         print "closed connection to:", addr
 
 
 if __name__ == '__main__':
+    sensor = BMP085.BMP085()
     soc = socket.socket()
     soc.bind(("localhost", 1337))
     soc.listen(5)  # max 5 waiting connections
