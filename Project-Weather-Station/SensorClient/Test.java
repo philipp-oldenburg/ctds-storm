2446e002424cac2c58774cf4db3a128b32a9f130
Philipp Oldenburg <philipp.oldenburg@stud.unibas.ch>
2015-11-08 23:16:07 +0100

    added supervisor configuration from Sensor Server

--- Project-Weather-Station/SensorClient/Test.java
+++ Project-Weather-Station/SensorClient/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		SensorClient client = new SensorClient("192.168.2.43");
+		SensorClient client = new SensorClient("192.168.2.66");
 		System.out.println(client.getTemperature());
 		System.out.println(client.getPressure());
 		System.out.println(client.getAltitude());
