655b65c109c900dcb26ebfd091e2dd2864029d48
Philipp Oldenburg <philipp.oldenburg@stud.unibas.ch>
2015-11-17 20:38:06 +0100

    small change to SensorClient

--- Project-Weather-Station/SensorClient/Test.java
+++ Project-Weather-Station/SensorClient/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		SensorClient client = new SensorClient("192.168.2.66");
+		SensorClient client = new SensorClient("2A02:8071:A3C5:6500:157E:E550:F64E:7534");
 		System.out.println(client.getTemperature());
 		System.out.println(client.getPressure());
 		System.out.println(client.getAltitude());
