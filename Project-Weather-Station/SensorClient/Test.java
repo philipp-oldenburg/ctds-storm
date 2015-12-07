e4de6d67f430ab13e3075c64810d0d2b2713dec0
Philipp Oldenburg <philipp.oldenburg@stud.unibas.ch>
2015-12-07 22:22:39 +0100

    updated SensorClient.java for humidity

--- Project-Weather-Station/SensorClient/Test.java
+++ Project-Weather-Station/SensorClient/Test.java
@@ -3,12 +3,17 @@ public class Test {
 
 	public static void main(String[] args) {
 		SensorClient client = new SensorClient("deffi.thecuslink.com");
-		long timestamp = System.currentTimeMillis();
+		System.out.println("TEMP");
 		System.out.println(client.getTemperature());
-		System.out.println(System.currentTimeMillis() - timestamp);
-		System.out.println(client.getPressure());
+		System.out.println("HUM");
+		System.out.println(client.getHumidity());
+		System.out.println("ALTI");
 		System.out.println(client.getAltitude());
+		System.out.println("PRESS");
+		System.out.println(client.getPressure());
+		System.out.println("SEAPRESS");
 		System.out.println(client.getSealevelPressure());
+		
 	}
 
 }
