4f358e76ca6ac6982e93b3e905f5dcdeb86c09d2
Philipp Oldenburg <philipp.oldenburg@stud.unibas.ch>
2015-12-08 23:12:16 +0000

    replaced light sensor library with a new one which is more stable

--- Project-Weather-Station/SensorServer/SensorServer.py
+++ Project-Weather-Station/SensorServer/SensorServer.py
@@ -1,7 +1,8 @@
 import socket
 import thread
 import Adafruit_BMP.BMP085 as BMP085
-
+from HDC1008 import HDC1008
+from tsl2561 import TSL2561
 
 def readline(): # ignores \r
     line = "";
@@ -21,13 +22,19 @@ def handler():
         while True:
             msg = readline()
             if msg == "TEMP":
-                conn.send('{0:0.2f}\n'.format(sensor.read_temperature()))
+		data = sensor2.pollsensordata()
+                conn.send('{0}\n'.format(data["temp"]))
             elif msg == "PRES":
-                conn.send('{0:0.2f}\n'.format(sensor.read_pressure()))
+                conn.send('{0}\n'.format(sensor.read_pressure()))
             elif msg == "ALTI":
-                conn.send('{0:0.2f}\n'.format(sensor.read_altitude()))
+                conn.send('{0}\n'.format(sensor.read_altitude()))
             elif msg == "SEAL":
-                conn.send('{0:0.2f}\n'.format(sensor.read_sealevel_pressure()))
+                conn.send('{0}\n'.format(sensor.read_sealevel_pressure()))
+            elif msg == "HUMI":
+                data = sensor2.pollsensordata()
+                conn.send('{0}\n'.format(data["humi"]))
+            elif msg == "LUMI":
+                conn.send('{0}\n'.format(sensor3.lux()))
             else:
                 print "received message in conflict with our protocol"
     except Exception as e:
@@ -37,6 +44,8 @@ def handler():
 
 if __name__ == '__main__':
     sensor = BMP085.BMP085()
+    sensor2 = HDC1008(0x40)
+    sensor3 = TSL2561()
     soc = socket.socket()
     soc.bind(("", 1337))
     soc.listen(5)  # max 5 waiting connections
