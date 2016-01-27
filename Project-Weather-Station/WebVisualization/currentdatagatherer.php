4c6b7c96148ac53675ba86ee9d315d5406ad7080
Philipp Oldenburg <philipp.oldenburg@stud.unibas.ch>
2016-01-27 16:26:13 +0100

    updated web

--- Project-Weather-Station/WebVisualization/currentdatagatherer.php
+++ Project-Weather-Station/WebVisualization/currentdatagatherer.php
@@ -1,9 +1,15 @@
 <?php
 	$fp = fsockopen("192.168.2.124", 9001, $errno, $errstr, 30);
+//	$fp = fsockopen("192.168.2.125", 9001, $errno, $errstr, 30);
 	if (!$fp) {
 		echo "$errstr ($errno)<br />\n";
 	} else {
-		$out = "NEWOWM\n";
+		$out = "CLASSOWM\n";
+		//echo $out;
+		fwrite($fp, $out);
+		echo fgets($fp);
+		echo ";";
+		$out = "CLASSSENS\n";
 		//echo $out;
 		fwrite($fp, $out);
 		echo fgets($fp);
