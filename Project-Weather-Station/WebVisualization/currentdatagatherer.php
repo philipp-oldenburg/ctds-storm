b417ed24ed82b23a89c80421f6e002ee8be81bae
Philipp Oldenburg <philipp.oldenburg@stud.unibas.ch>
2016-01-28 13:43:59 +0100

    [web] light can be plotted

--- Project-Weather-Station/WebVisualization/currentdatagatherer.php
+++ Project-Weather-Station/WebVisualization/currentdatagatherer.php
@@ -1,6 +1,5 @@
 <?php
 	$fp = fsockopen("192.168.2.124", 9001, $errno, $errstr, 30);
-//	$fp = fsockopen("192.168.2.125", 9001, $errno, $errstr, 30);
 	if (!$fp) {
 		echo "$errstr ($errno)<br />\n";
 	} else {
